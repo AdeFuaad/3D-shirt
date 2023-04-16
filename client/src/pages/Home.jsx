df710a0a929ce7bfc762daf43f404b679282fea0
AdeFuaad <adesholafuaad@gmail.com>
2023-04-15 20:36:15 -0400

    3D react app

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,37 +15,40 @@ const Home = () => {
 
   return (
     <AnimatePresence>
-      <motion.section className="home" {...slideAnimation("left")}>
-        <motion.header {...slideAnimation("down")}>
-          <img
-            src="./threejs.png"
-            alt="logo"
-            className="w-8 h-8 object-contain"
-          />
-        </motion.header>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
 
-        <motion.div className="home-content" {...headContainerAnimation}>
-          <motion.div {...headTextAnimation}>
-            <h1 className="head-text">
-              MAKE <br className="xl:block hidden" /> IT YOURS.
-            </h1>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                MAKE <br className="xl:block hidden" /> IT YOURS.
+              </h1>
+            </motion.div>
+
+            <motion.div {...headTextAnimation} className="flex flex-col gap-5">
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Design your distinguished t-shirt design with the 3D
+                customization tool. <strong>Visualize your architecture</strong>{" "}
+                and own it.
+              </p>
+
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
           </motion.div>
-        </motion.div>
-
-        <motion.div {...headTextAnimation} className="flex flex-col gap-5">
-          <p className="max-w-md font-normal text-gray-600 text-base">
-            Design your distinguished t-shirt design with the 3D customization
-            tool. <strong>Visualize your architecture</strong> and own it.
-          </p>
-
-          <CustomButton
-            type="filled"
-            title="Customize It"
-            handleClick={() => state.intro = false}
-            customStyles="w-fit px-4 py-2.5 font-bold text-sm"
-          />
-        </motion.div>
-      </motion.section>
+        </motion.section>
+      )}
     </AnimatePresence>
   );
 };
